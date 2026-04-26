7e6a36d34ca1edeb875580881ead964d0f0d0ab7
tnoshin <tnoshin18@gmail.com>
2026-04-26 10:39:35 +0600

    Update booking.js

--- booking.js
+++ booking.js
@@ -1,26 +1,27 @@
 function toggleMenu() {
-    document.querySelector('.rest').classList.toggle('open'); // changed 'active' to 'open'
+    document.querySelector('.rest').classList.toggle('open');
 }
-document.querySelector(`form`).addEventListener(`submit`, function (event) {
-    if (document.querySelector(`#Name`).value === ``) {
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    if (document.querySelector('#Name').value === '') {
         event.preventDefault();
-        alert(`Please fill in your first name.`);
-    } else if (document.querySelector('#surname').value === ''){
+        alert('Please fill in your first name.');
+    } else if (document.querySelector('#surname').value === '') {
         event.preventDefault();
-        alert('Please fill in your last name.')   
-    } else if (document.querySelector('#time').value === ``){
+        alert('Please fill in your last name.');
+    } else if (document.querySelector('#time').value === '') {
         event.preventDefault();
-        alert('PLease provide the time you would like to take an appointment for.');
-    } else if (document.querySelector(`#number`).value === ``) {
+        alert('Please provide the time for your appointment.');
+    } else if (document.querySelector('#number').value === '') {
         event.preventDefault();
-        alert(`Please fill in your number.`);
-    } else if (document.querySelector(`#age`).value === ``) {
+        alert('Please fill in your number.');
+    } else if (document.querySelector('#age').value === '') {
         event.preventDefault();
-        alert(`Please provide your age.`);
-    }else if (document.querySelector(`#date`).value === ``) {
+        alert('Please provide your age.');
+    } else if (document.querySelector('#date').value === '') {
         event.preventDefault();
-        alert(`Please select a date, we are available all days of the week except Sunday.`);
+        alert('Please select a date.');
+    } else {
+        alert('Your appointment has been submitted successfully!');
     }
-    alert('Your form has been submitted successfully!');
-    form.reset();
-})
+});
